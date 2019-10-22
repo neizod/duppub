09bd1bd435c25576bf3e2c0970e0ecd4a5e6ff6a
nickalaskreynolds <nickalaskreynolds@gmail.com>
2019-10-22 15:35:44 -0500

    minor linting and changing from argparse to docopt

--- report.py
+++ report.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3
 
-import argparse
+"""Usage:
+  report.py [-t N] [-l N] [--algorithm S] <CSV>
+
+Detect duplicate or similar publications from database. This project aim to reduce size of the database by showing pairs of suspect duplications, to help citation easier and cleaner.
+
+Export database as CSV file without header, with these fields: ID, Authors, Title of Article, Year, Abstract
+
+Arguments:
+  CSV                      Input CSV file to process
+
+Options:
+  -h --help                show this help message and exit
+  -v, --version            show version and exit
+  -t, --threshold N        Threshold percentage, as an integer [default: 80]
+  -l, --limit_chars N      String length limit to increase performance, as an integer [default: 100]
+  --algorithm S            Algorithm, as a string [default: levenshtein edit_distance cosine_similarity]
+
+Try:
+  ./report.py publications.csv
+  ./report.py -t 10 publications.csv
+  ./report.py -t 10 -l 100 publications.csv
+  ./report.py -t 10 -l 100 --algorithm edit_distance publications.csv
+"""
+
 import csv
 import math
 import re
 from collections import Counter
 from enum import IntEnum
 
+from docopt import docopt
 import numpy as np
 
 
@@ -23,23 +47,23 @@ class Field(IntEnum):
 
 
 def levenshtein_ratio(s, t):
-    rows = len(s)+1
-    cols = len(t)+1
-    distance = np.zeros((rows,cols),dtype = int)
+    rows = len(s) + 1
+    cols = len(t) + 1
+    distance = np.zeros((rows, cols), dtype=int)
     for i in range(1, rows):
-        for k in range(1,cols):
+        for k in range(1, cols):
             distance[i][0] = i
             distance[0][k] = k
     for col in range(1, cols):
         for row in range(1, rows):
-            if s[row-1] == t[col-1]:
+            if s[row - 1] == t[col - 1]:
                 cost = 0
             else:
-                cost =2
-            distance[row][col] = min( distance[row-1][col] + 1,
-                                      distance[row][col-1] + 1,
-                                      distance[row-1][col-1] + cost )
-    ratio = ((len(s)+len(t)) - distance[row][col]) / (len(s)+len(t))
+                cost = 2
+            distance[row][col] = min(distance[row - 1][col] + 1,
+                                     distance[row][col - 1] + 1,
+                                     distance[row - 1][col - 1] + cost)
+    ratio = ((len(s) + len(t)) - distance[row][col]) / (len(s) + len(t))
     return 100 * ratio
 
 
@@ -53,7 +77,7 @@ def edit_distance_ratio(this_str, other_str):
             if c1 == c2:
                 new_distant += [distance[i1]]
             else:
-                new_distant += [1 + min(distance[i1], distance[i1+1], new_distant[-1])]
+                new_distant += [1 + min(distance[i1], distance[i1 + 1], new_distant[-1])]
         distance = new_distant
     return 100 - (100 * distance[-1] / max(len(this_str), len(other_str)))
 
@@ -65,7 +89,7 @@ def cosine_similarity_ratio(this_str: str, other_str: str) -> float:
     :param other_str:
     :return:
     """
-    def get_cosine(vec1: Counter, vec2: Counter) ->float:
+    def get_cosine(vec1: Counter, vec2: Counter) -> float:
         """
         calculate cosine similarity
         :param vec1:
@@ -115,9 +139,9 @@ def process(arguments):
                 continue
             if this_id > other_id:
                 continue
-            table[this_id][other_id] = difference( this_row, other_row,
-                                                   limit_chars=arguments.limit_chars,
-                                                   distance_algorithm=arguments.algorithm )
+            table[this_id][other_id] = difference(this_row, other_row,
+                                                  limit_chars=arguments.limit_chars,
+                                                  distance_algorithm=arguments.algorithm)
     report(table, arguments.threshold)
 
 
@@ -136,24 +160,51 @@ def report(table, percent=80):
         print(f'| {line[0]:>6.2f}% | {line[1]:<22} | {line[2]:<22} |')
 
 
+def merge(dict_1, dict_2):
+    """Merge two dictionaries.
+    Values that evaluate to true take priority over falsy values.
+    `dict_1` takes priority over `dict_2`.
+    """
+    return dict((str(key).replace('<CSV>', 'csv').replace('--', ''),
+                dict_1.get(key) or dict_2.get(key)) for key in set(dict_2) | set(dict_1))
+
+
+class Struct:
+    """Dummy class to construct objects from dictionaries."""
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+
 def parse_arguments():
+    """Parse input arguments."""
+    inputs = docopt(__doc__, version='0.0.1')
+
+    # Implemented Algorithms
     algorithms = {
         'levenshtein': levenshtein_ratio,
         'edit_distance': edit_distance_ratio,
         'cosine_distance': cosine_similarity_ratio
     }
-    parser = argparse.ArgumentParser(description='DupPub detects duplicate publications')
-    parser.add_argument('csv', type=str,  help='CSV file to process')
-    parser.add_argument('--threshold', type=int, default=80, help='Threshold percentage, as an integer.')
-    parser.add_argument('--limit_chars', type=int, default=100, help='String length limit to increase performance.')
-    parser.add_argument('--algorithm', type=str, default='levenshtein', help='The algorithm to use.')
-    arguments = parser.parse_args()
-    if not 0 <= arguments.threshold <= 100:
-        parser.error('THRESHOLD not in range 0% to 100%')
-    if arguments.algorithm in algorithms.keys():
-        arguments.algorithm = algorithms[arguments.algorithm]
-    else:
-        parser.error('ALGORITHM not exists')
+    # Fallback values for input
+    defaults = {
+        '--threshold': 80,
+        '--limit_chars': 100,
+        '--algorithm': 'levenshtein'
+
+    }
+    # merge values together with defaults as fallback
+    args = merge(inputs, defaults)
+    arguments = Struct(**args)
+    # recast and verify arguments integrity
+    try:
+        arguments.threshold = int(arguments.threshold)
+        arguments.limit_chars = int(arguments.limit_chars)
+        assert arguments.threshold <= 100 and arguments.threshold > 0
+        assert arguments.algorithm in algorithms.keys()
+    except Exception as e:
+        print(e)
+        raise ValueError("Invalid arguments either out of range or not found in list.")
+    arguments.algorithm = algorithms[arguments.algorithm]
     return arguments
 
 
@@ -163,3 +214,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# end of code
